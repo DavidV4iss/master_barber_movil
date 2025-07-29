d1b73cb041b7b564959d4a080ca6bb3f8693310c
Fidel-Smoke <fideljoseespi10@gmail.com>
2025-07-29 10:53:33 -0500

    Nos Fuimos

--- src/config/api.js
+++ src/config/api.js
@@ -1,24 +1,18 @@
-// api.js
-
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from 'react-native';
 import Constants from 'expo-constants';
 
 export const getBaseURL = () => {
-  // 👉 Siempre usar la API en Render si estamos en producción o en web
   if (process.env.NODE_ENV === 'production' || Platform.OS === 'web') {
-    return 'https://api-de-master-barber.onrender.com';
+    return 'https://master-barber-api.onrender.com';
   }
 
-  // 👉 Si estás usando Expo Go en físico o emulador (desarrollo local)
   if (Constants.expoConfig?.hostUri) {
     const ip = Constants.expoConfig.hostUri.split(':')[0];
     return `http://${ip}:8080/`;
   }
 
-  // Fallback por defecto (opcional)
-  return 'http://localhost:8080/';
 };
 
 const API = axios.create({
@@ -26,7 +20,6 @@ const API = axios.create({
   timeout: 10000,
 });
 
-// Interceptor para incluir token JWT en las peticiones (si existe)
 API.interceptors.request.use(async (config) => {
   try {
     const token = await AsyncStorage.getItem('token');
