98360b80b25b71fbff56aba553ab2db4409e4e2b
Fidel-Smoke <fideljoseespi10@gmail.com>
2025-07-30 12:36:00 -0500

    Funcional X2

--- src/config/api.js
+++ src/config/api.js
@@ -4,23 +4,19 @@ import { Platform } from 'react-native';
 import Constants from 'expo-constants';
 
 export const getBaseURL = () => {
-  // 🌐 Desarrollo en web (React Native Web)
   if (Platform.OS === 'web' && process.env.NODE_ENV !== 'production') {
     return 'http://localhost:8080';
   }
 
-  // 🚀 Producción (web o móvil)
   if (process.env.NODE_ENV === 'production') {
     return 'https://master-barber-api-va0x.onrender.com';
   }
 
-  // 📱 Desarrollo en móvil (Expo Go o emulador)
   if (Constants.expoConfig?.hostUri) {
     const ip = Constants.expoConfig.hostUri.split(':')[0];
     return `http://${ip}:8080/`;
   }
 
-  // 🔁 Fallback por si nada aplica
   return 'http://localhost:8080';
 };
 
